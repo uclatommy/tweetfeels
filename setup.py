0e98529129a9bb3f3ebcc2dfd6f9324d11d8a945
Thomas Chen <tkchen@gmail.com>
2017-02-21 20:02:25 -0800

    Fill out setup.py for pypi. Also vaderSentiment doesn't come with dataset unless you install from repo.

--- setup.py
+++ setup.py
@@ -8,5 +8,22 @@ setup(name='tweetfeels',
       author_email='tkchen@gmail.com',
       url='https://github.com/uclatommy/tweetfeels',
       download_url='https://github.com/uclatommy/tweetfeels/tarball/0.1.3',
-      packages=['tweetfeels']
+      packages=['tweetfeels'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+          ]
+      install_requires=[
+          'tweepy', 'h5py', 'nltk', 'numpy', 'oauthlib', 'pandas',
+          'python-dateutil', 'pytz', 'requests', 'requests-oauthlib',
+          'six', 'twython'
+          ],
+      dependency_links=[
+          'https://github.com/cjhutto/vaderSentiment/tarball/0.5'
+          ],
+      test_suite='nose.collector',
+      tests_require=['nose']
      )
