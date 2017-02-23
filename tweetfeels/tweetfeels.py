b38e6285f5ebd91f7fdf656a60bdc81355f5021b
Thomas Chen <tkchen@gmail.com>
2017-02-22 22:07:44 -0800

    Fix deprecation.

--- tweetfeels/tweetfeels.py
+++ tweetfeels/tweetfeels.py
@@ -13,11 +13,11 @@ def clean(text):
     text = text.lower()
     text = sub("[0-9]+", "number", text)
     text = sub("#", "", text)
-    text = sub("\n", "", text)
+    text = sub("\\n", "", text)
     text = text.replace('$', '@')
-    text = sub("@[^\s]+", "", text)
-    text = sub("(http|https)://[^\s]*", "", text)
-    text = sub("[^\s]+@[^\s]+", "", text)
+    text = sub("@[^\\s]+", "", text)
+    text = sub("(http|https)://[^\\s]*", "", text)
+    text = sub("[^\\s]+@[^\\s]+", "", text)
     text = sub('[^a-z A-Z]+', '', text)
     return text
 
