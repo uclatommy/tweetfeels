uclatommy/tweetfeels

commit 45f610398f2e422653178459e4610d1900378d0c

This is the most efficient way to get the last sentiment value from the generator.

tweetfeels/tweetfeels.py
@@ -82,11 +82,9 @@ class TweetFeels(object):
         sentiments = self.sentiments(
             strt=self._latest_calc, end=end, delta_time=self._bin_size
             )
-        ret = np.nan
         for s in sentiments:
-            if not np.isnan(s):
-                ret = s
-        return ret
+            pass
+        return s
 
     def start(self, seconds=None, selfupdate=60):
         """

commit 5791a89e64ce94ff6b984cee0fb4ae534aa9b42c

Add default sentiment (None) to fix #35
Looks like a bug was introduced in 45f610398f2e422653178459e4610d1900378d0c. The value of `s` is defined in the for loop, so if the generator is empty, s never gets that definition and results in the UnboundLocalError.

```
@property
def sentiment(self):
    end = self._feels.end
    sentiments = self.sentiments(
        strt=self._latest_calc, end=end, delta_time=self._bin_size
        )
    for s in sentiments:
        pass
    return s
```
I initialized `s` as None (which seems reasonable to me).

tweetfeels/tweetfeels.py
@@ -128,6 +128,7 @@ class TweetFeels(object):
         sentiments = self.sentiments(
             strt=self._latest_calc, end=end, delta_time=self._bin_size
             )
+        s = None
         for s in sentiments:
             pass
         return s